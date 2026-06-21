e71ddcf151f63919a366e251abc8e9acba4a38c7
Alexander <aburlaka@rambler.ru>
2026-06-21 18:56:16 +0300

    with third allure

--- pipelines/api_tests.groovy
+++ pipelines/api_tests.groovy
@@ -10,15 +10,15 @@ timeout(120) {
      }
      stage("Build Docker image") {
          docker.withRegistry("http://localhost:5005") { //в Registry, который прилагается к дженкинсу
-             docker.build("otus:allure:${env.BRANCH}").push() //собираем докер-образ с именем(имя проекта:имя ветки) и пушим его в Registry для дальнейшего запуска
+             docker.build("root/api_tests:${env.BRANCH}").push() //собираем докер-образ с именем(имя проекта:имя ветки) и пушим его в Registry для дальнейшего запуска
              //docker.build("api_tests:1.1").push()
          }
      }
      stage("Running api-tests") {
         ansiblePlaybook playbook: "playbook.yml", //плейбука, которая запускает тесты (и разворачивает инфраструктуру)
                 extraVars: [
-                        branch : "${env.BRANCH}", //ветка, из которой запускаем
-                        profile: "${params.PROFILE}" //какие именно тесты запускаем (ui/api/appium)
+                        branch : "${env.BRANCH}", //передаем в плейбуку ветку, из которой запускаем
+                        profile: "${params.PROFILE}" //передаем в плейбуку, какие именно тесты запускаем (ui/api/appium)
                 ]
         //sh "docker run --rm tests_api:1.0"
      }
