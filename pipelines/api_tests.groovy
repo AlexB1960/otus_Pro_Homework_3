78186a10c37f874b84983832bd445b40004fdf24
Alexander <aburlaka@rambler.ru>
2026-06-22 14:39:33 +0300

    with ninth allure

--- pipelines/api_tests.groovy
+++ pipelines/api_tests.groovy
@@ -5,23 +5,39 @@ timeout(120) {
 
      def params = readYaml text: env.YAML_CONFIG ?: [:] //считываем YAML-параметры
 
-     stage("Checkout") {
-         checkout scm  //стягиваем проект
+     try {
+         stage("Checkout") {
+             checkout scm  //стягиваем проект
+         }
+         stage("Build Docker image") {
+             docker.withRegistry("http://localhost:5005") { //в Registry, который прилагается к дженкинсу
+                 docker.build("otus-allure:${env.REFSPEC}").push() //собираем докер-образ с именем(имя проекта:имя ветки) и пушим его в Registry для дальнейшего запуска
+                 //docker.build("api_tests:1.1").push()
+             }
+         }
+     }  finally {
+         cleanWs(deleteDirs: true)
      }
-     stage("Build Docker image") {
-         docker.withRegistry("http://localhost:5005") { //в Registry, который прилагается к дженкинсу
-             docker.build("otus-allure:${env.REFSPEC}").push() //собираем докер-образ с именем(имя проекта:имя ветки) и пушим его в Registry для дальнейшего запуска
-             //docker.build("api_tests:1.1").push()
+
+     dir("${params.PROFILE}") {
+         try {
+             stage("Checkout") {
+                 checkout scm  //стягиваем проект
+             }
+             stage("Running api-tests") {
+                 ansiblePlaybook playbook: "playbook.yml", //плейбука, которая запускает тесты (и разворачивает инфраструктуру)
+                         extraVars: [
+                                 branch : "${env.REFSPEC}", //передаем BRANCH в плейбуку ветку, из которой запускаем
+                                 profile: "${params.PROFILE}" //передаем в плейбуку, какие именно тесты запускаем (ui/api/appium)
+                         ]
+                 //sh "docker run --rm tests_api:1.0"
+             }
+         } finally {
+             deleteDir()
          }
      }
-     stage("Running api-tests") {
-        ansiblePlaybook playbook: "playbook.yml", //плейбука, которая запускает тесты (и разворачивает инфраструктуру)
-                extraVars: [
-                        branch : "${env.REFSPEC}", //передаем BRANCH в плейбуку ветку, из которой запускаем
-                        profile: "${params.PROFILE}" //передаем в плейбуку, какие именно тесты запускаем (ui/api/appium)
-                ]
-        //sh "docker run --rm tests_api:1.0"
-     }
+
+
      stage("Allure report") {
          sh "tar -czf ajjure-results.tar.gz -C allure-results ." //архивация json-файлов текущещей джобы в tar-архив
          archiveArtifacts artifacts: "*.tar.gz", //пушим архив как артифакт текущей джобы
