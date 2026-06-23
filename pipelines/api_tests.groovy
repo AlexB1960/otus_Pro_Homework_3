2f32139de16bd3d06cbd0ae0e47f60223c5c98e9
Alexander <aburlaka@rambler.ru>
2026-06-23 11:12:24 +0300

    without otus-allure

--- pipelines/api_tests.groovy
+++ pipelines/api_tests.groovy
@@ -25,12 +25,13 @@ timeout(120) {
                  checkout scm  //стягиваем проект
              }
              stage("Running api-tests") {
-                 ansiblePlaybook playbook: "playbook.yml", //плейбука, которая запускает тесты (и разворачивает инфраструктуру)
-                         extraVars: [
-                                 branch : "${env.REFSPEC}", //передаем BRANCH в плейбуку ветку, из которой запускаем
-                                 profile: "${params.PROFILE}" //передаем в плейбуку, какие именно тесты запускаем (ui/api/appium)
-                         ]
+                 //ansiblePlaybook playbook: "playbook.yml", //плейбука, которая запускает тесты (и разворачивает инфраструктуру)
+                 //        extraVars: [
+                 //                branch : "${env.REFSPEC}", //передаем BRANCH в плейбуку ветку, из которой запускаем
+                 //                profile: "${params.PROFILE}" //передаем в плейбуку, какие именно тесты запускаем (ui/api/appium)
+                 //        ]
                  //sh "docker run --rm tests_api:1.0"
+                 sh "docker run --rm api_tests:1.1"
              }
          } finally {
              deleteDir()
